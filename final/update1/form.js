6cc35a2eb35415bc36e800598cf6d1266b8144f0
Yootak Kim <kimyootak96@gmail.com>
2025-04-11 17:32:30 +0900

    add finalproject

--- final/update1/form.js
+++ final/update1/form.js
@@ -20,8 +20,23 @@ document.getElementById("dogOrderForm").addEventListener("submit", function (e) 
         }
         document.getElementById("dogOrderForm").reset();
       })
-      .catch((error) => {
-        console.error("Error fetching response.json:", error);
-        document.getElementById("confirmation").textContent = "Something went wrong. Please try again later.";
-      });
-  });
+      // .catch((error) => {
+      //   console.error("Error fetching response.json:", error);
+      //   document.getElementById("confirmation").textContent = "Something went wrong. Please try again later.";
+      // });
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', 'response.json', true);
+      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          document.getElementById("message").innerHTML = response.message;
+          document.getElementById("dogOrderForm").reset();
+        } else if (xhr.readyState === 4) {
+          alert('Form submission failed. Try again.');
+        } 
+      };
+
+      xhr.send(JSON.stringify(data));
+      console.log(data);
+    });
